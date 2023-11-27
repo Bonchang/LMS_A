a9830c20b805811d165d7fba6ee97d5d21cbc029
Bonchang <9bonbon@naver.com>
2023-11-27 13:02:01 +0900

    1127-1

---
+++ LMS_A/src/LDirectoryPanel.java
@@ -0,0 +1,38 @@
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.awt.BorderLayout;
+import java.io.File;
+
+public class LDirectoryPanel extends JPanel {
+    private JTree directoryTree;
+
+    public LDirectoryPanel() {
+        this.setLayout(new BorderLayout());
+
+        // 현재 디렉토리를 기준으로 트리 노드 생성
+        File currentDirectory = new File("."); // 현재 디렉토리 (".")
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(currentDirectory.getName());
+        createNodes(root, currentDirectory); // 하위 디렉토리와 파일들을 트리에 추가
+
+        // 트리 모델 생성 및 설정
+        directoryTree = new JTree(root);
+        this.add(directoryTree, BorderLayout.CENTER);
+    }
+
+    private void createNodes(DefaultMutableTreeNode node, File file) {
+        if (file.isDirectory()) {
+            // 디렉토리인 경우, 하위 파일과 디렉토리를 탐색
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File child : files) {
+                    DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(child.getName());
+                    node.add(childNode);
+                    createNodes(childNode, child); // 재귀적으로 하위 노드 추가
+                }
+            }
+        }
+        // 파일인 경우, 추가 로직은 필요하지 않음
+    }
+}
--- LMS_A/src/LMainFrame.java
+++ LMS_A/src/LMainFrame.java
@@ -1,34 +1,22 @@
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.LayoutManager;
-
 import javax.swing.JFrame;
-
-import javax.swing.JFrame;
-import javax.swing.JTree;
-import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
 public class LMainFrame extends JFrame {
     private static final long serialVersionUID = 1L;
 
-    private JPanel directoryPanel;
-    private JTree directoryTree;
+    private LDirectoryPanel directoryPanel;
     private LMenuBar menuBar;
     private LMainPanel mainPanel;
 
     public LMainFrame() {
         // Set attributes
-        this.setSize(800, 600); // 사이즈를 조금 더 크게 조정
+        this.setSize(800, 600);
 
         this.setLayout(new BorderLayout());
 
         // Directory panel
-        this.directoryTree = new JTree(); // 디렉토리 구조를 표시할 JTree
-        this.directoryPanel = new JPanel();
-        this.directoryPanel.setLayout(new BorderLayout());
-        this.directoryPanel.add(directoryTree, BorderLayout.CENTER);
-        this.add(this.directoryPanel, BorderLayout.WEST); // 왼쪽에 패널 추가
+        this.directoryPanel = new LDirectoryPanel();
+        this.add(this.directoryPanel, BorderLayout.WEST);
 
         // Menu bar
         this.menuBar = new LMenuBar();
@@ -38,5 +26,8 @@ public class LMainFrame extends JFrame {
         this.mainPanel = new LMainPanel();
         this.add(this.mainPanel, BorderLayout.CENTER);
     }
+
+    // 필요한 경우 추가 메소드 구현
 }
+
 
